aff5553f78d19d8837fdb0848e83a862796136b6
Qréon <francois.brachais@free.fr>
2017-06-11 22:47:35 +0200

    Update FimuGoer and AbstractAnimat, add Need
    Made AbstractAnimat actually abstract by definition, added abstract methods as well, prepared FimuGoer for implementation, added the Need enumeration to categorize needs in agents.

---
+++ src/main/sarl/framework/agent/Need.java
@@ -0,0 +1,19 @@
+package framework.agent;
+
+public enum Need {
+	WATER("to drink"),
+	FOOD("to eat"),
+	TOILETS("to pee"),
+	CONCERT("to attend a concert"),
+	TALK("to have a nice conversation with other people");
+	
+	String sentenceFragment = "";
+	
+	Need(String sentenceFragment) {
+		this.sentenceFragment = sentenceFragment;
+	}
+	
+	public String toString() {
+		return sentenceFragment;
+	}
+}
